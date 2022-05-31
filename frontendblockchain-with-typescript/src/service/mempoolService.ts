b4ff93b8b80b0578910ae502336e252505626f97
Abigail González E <pamelagon12@gmail.com>
2022-05-30 18:31:56 -0600

    Mempool service, enviar archivos e informacion incompleto

--- frontendblockchain-with-typescript/src/service/mempoolService.ts
+++ frontendblockchain-with-typescript/src/service/mempoolService.ts
@@ -1,12 +1,13 @@
 
 import axios from "axios";
-import Mempool from "../types/mempool.type";
+import MempoolS from "../types/mempool.type";
 
-export default function registerMempool (mempool: Mempool)  {   
+export default function registerMempool (mempool: MempoolS)  {   
 
-    const response=  axios.post<Mempool>('https://localhost:44317/api/Usuario/',mempool).then(response => response.data)  
+    console.log(mempool);
+    const response=  axios.post<MempoolS>('https://localhost:44317/api/Mempool',mempool).then(response => response.data)  
    // alertMessage(SUCCESS_MESSAGE_REGISTER,ICON_SUCCESS);
-  
+
  return response
 
 
